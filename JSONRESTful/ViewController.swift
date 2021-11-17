28c7fa5a14c092aeab8cbbeb6b34fb4b3493eee7
paquito05 <samuel.john.11.jsas@gmail.com>
2021-11-17 12:16:24 -0800

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by John Samuel Altamirano Sanchez on 11/17/21.
+//  Copyright © 2021 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
